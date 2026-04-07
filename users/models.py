9a92cea0e540d5b22f9079bdb5a13e40ccc5b255
4JN22CS147 <gowdasharmadar@gmail.com>
2026-04-07 10:15:24 +0530

    fixed user model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,51 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, phone=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            phone=phone,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, phone=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, phone, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    objects = UserManager()   # 🔥 VERY IMPORTANT
+
+    def __str__(self):
+        return self.email
+
+
+
+"""from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    username = None   # ❌ remove username
+    username = None  
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=10, unique=True)
 
@@ -10,4 +53,6 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['phone']
 
     def __str__(self):
-        return self.email
+        return self.email"""
+    
+    
